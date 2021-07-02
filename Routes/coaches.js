6bc27b11b6a665f514d09cb1aa3d8d035487dfc9
Tanish Vemu <www.vtani74@gmail.com>
2021-07-02 17:06:29 +0530

    register route

--- Routes/coaches.js
+++ Routes/coaches.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 
 const Coach = require("../models/Coach");
@@ -19,14 +22,57 @@ router.post(
       min: 6,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({
         errors: errors.array(),
       });
     }
-    res.send("Passed");
+    const { name, phone, password, teamName, sport } = req.body;
+
+    try {
+      let team = await Coach.findOne({ teamName });
+      if (team) {
+        return res.status(400).json({
+          msg: "Team already exists, Please choose another team name",
+        });
+      }
+      coach = new Coach({
+        name,
+        phone,
+        teamName,
+        sport,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      coach.password = await bcrypt.hash(password, salt);
+
+      await coach.save();
+
+      const payload = {
+        coach: {
+          id: coach.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtsecret"),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).json({ msg: "server error" });
+    }
   }
 );
 
