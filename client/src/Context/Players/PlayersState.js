a8e279bafbed9bbed1c69894b133b508e0a818ce
raviSridhar55 <61363065+raviSridhar55@users.noreply.github.com>
2021-07-15 00:33:32 +0530

    dummy data

--- client/src/Context/Players/PlayersState.js
+++ client/src/Context/Players/PlayersState.js
@@ -3,36 +3,51 @@ import playerContext from './playerContext';
 import playerReducer from './playerReducer';
 
 const PlayerState = (props) => {
-  const initialState = [
-    {
-      name: 'Mason Greenwood',
-      playerNumber: '11',
-      role: 'Striker',
-      status: 'Substitute',
-    },
-    {
-      name: 'Harry Kane',
-      playerNumber: '9',
-      role: 'Striker',
-      status: 'Playing',
-    },
-    {
-      name: 'Heung Min Son',
-      playerNumber: '7',
-      role: 'Left Mid',
-      status: 'Playing',
-    },
-    {
-      name: 'Lionel Messi',
-      playerNumber: '10',
-      role: 'Midfielder',
-      status: 'Rest',
-    },
-    {
-      name: 'Christiano Ronaldo',
-      playerNumber: '7',
-      role: 'Striker',
-      status: 'Playing',
-    },
-  ];
+  const initialState = {
+    players: [
+      {
+        name: 'Mason Greenwood',
+        playerNumber: '11',
+        role: 'Striker',
+        status: 'Substitute',
+      },
+      {
+        name: 'Harry Kane',
+        playerNumber: '9',
+        role: 'Striker',
+        status: 'Playing',
+      },
+      {
+        name: 'Heung Min Son',
+        playerNumber: '7',
+        role: 'Left Mid',
+        status: 'Playing',
+      },
+      {
+        name: 'Lionel Messi',
+        playerNumber: '10',
+        role: 'Midfielder',
+        status: 'Rest',
+      },
+      {
+        name: 'Christiano Ronaldo',
+        playerNumber: '7',
+        role: 'Striker',
+        status: 'Playing',
+      },
+    ],
+  };
+
+  const [state, dispatch] = useReducer(playerReducer, initialState);
+
+  return (
+    <playerContext.Provider
+      value={{
+        players: state.players,
+      }}>
+      {props.children}
+    </playerContext.Provider>
+  );
 };
+
+export default PlayerState;
