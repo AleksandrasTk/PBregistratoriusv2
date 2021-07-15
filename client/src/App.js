a5e2879651735b42cb2c4968aa94b855eb721149
raviSridhar55 <61363065+raviSridhar55@users.noreply.github.com>
2021-07-16 00:06:59 +0530

    Design

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import "./App.css";
-import "./main.css";
+import './App.css';
+import './main.css';
 // import Playeritems from "./Components/player/Playeritems";
-import Navbar from "./Components/Layout/Navbar";
-import Playerform from "./Components/player/Playerform";
-import Playersstate from "./Context/Players/PlayersState";
-import Landing from "./Components/Layout/Landing";
+import Navbar from './Components/Layout/Navbar';
+// import Playerform from "./Components/player/Playerform";
+import Playersstate from './Context/Players/PlayersState';
+import Landing from './Components/Layout/Landing';
 // import Tabs from "./Components/Layout/Tabs";
 
 function App() {
