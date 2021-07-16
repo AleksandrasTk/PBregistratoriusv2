028a29214417036145e124f0bcb8c8f43a07e9b6
Soumya Kesarapu <soumyacole5566@gmail.com>
2021-07-17 00:52:10 +0530

    Routing

--- client/src/Components/player/playerform.js
+++ client/src/Components/player/playerform.js
@@ -1,60 +1,60 @@
-import React from 'react';
+import React from "react";
 
 const Playerform = () => {
   const onSubmit = (e) => {
-    console.log('hello');
+    console.log("hello");
     e.preventDefault();
   };
   return (
-    <div className='container player-form'>
-      <form className='add-form' onSubmit={onSubmit}>
-        <h1 style={{ textAlign: 'center', color: 'black' }}>
+    <div className="container player-form">
+      <form className="add-form" onSubmit={onSubmit}>
+        <h1 style={{ textAlign: "center", color: "black" }}>
           Add a player to your sqaud
         </h1>
-        <div className='form-control'>
+        <div className="form-control">
           <label>NAME</label>
-          <input type='text' placeholder='Player Name' />
+          <input type="text" placeholder="Player Name" />
         </div>
-        <div className='form-control'>
+        <div className="form-control">
           <label>PLAYER NUMBER</label>
-          <input type='number' placeholder='Player Number' />
+          <input type="number" placeholder="Player Number" />
         </div>
-        <div className='form-control'>
+        <div className="form-control">
           <label>ROLE/POSITION</label>
-          <input type='text' placeholder='Player Position' />
+          <input type="text" placeholder="Player Position" />
         </div>
 
-        <div className='form-control form-control-check'>
+        <div className="form-control form-control-check">
           <label>STATUS:</label>
-          <div className='status-radio'>
+          <div className="status-radio">
             <input
-              type='radio'
-              name='type'
-              value='personal'
+              type="radio"
+              name="type"
+              value="personal"
               // checked={type === 'personal'}
             />
-            <label htmlFor='Playing'>PLaying</label>
+            <label htmlFor="Playing">PLaying</label>
           </div>
-          <div className='status-radio'>
+          <div className="status-radio">
             <input
-              type='radio'
-              name='type'
-              value='personal'
+              type="radio"
+              name="type"
+              value="personal"
               // checked={type === 'personal'}
             />
-            <label htmlFor='Playing'>Substitue</label>
+            <label htmlFor="Playing">Substitue</label>
           </div>
-          <div className='status-radio'>
+          <div className="status-radio">
             <input
-              type='radio'
-              name='type'
-              value='personal'
+              type="radio"
+              name="type"
+              value="personal"
               // checked={type === 'personal'}
             />
-            <label htmlFor='Playing'>Rest</label>
+            <label htmlFor="Playing">Rest</label>
           </div>
         </div>
-        <button className='btn btn-block'>Add Player</button>
+        <button className="btn btn-block">Add Player</button>
       </form>
     </div>
   );
