662027d93b6bf440c8fb6d056bd7c7d56a433471
raviSridhar55 <61363065+raviSridhar55@users.noreply.github.com>
2021-07-16 22:12:38 +0530

    Changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import './main.css';
-// import Playeritems from "./Components/player/Playeritems";
+import Playeritems from './Components/player/Playeritems';
 import Navbar from './Components/Layout/Navbar';
-// import Playerform from "./Components/player/Playerform";
+import Playerform from './Components/player/Playerform';
 import Playersstate from './Context/Players/PlayersState';
 import Landing from './Components/Layout/Landing';
 // import Tabs from "./Components/Layout/Tabs";
+
+import Login from './Components/Auth/Login';
 
 function App() {
   return (
@@ -14,9 +16,10 @@ function App() {
         <Navbar />
         {/* <Tabs /> */}
       </div>
-      {/* <Playerform />
-      <Playeritems /> */}
+      <Playerform />
+      <Playeritems />
       <Landing />
+      <Login />
     </Playersstate>
   );
 }
