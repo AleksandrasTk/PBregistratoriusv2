5c09ccb797102c6b32f2b6aba2f0f70ed2e786ec
Soumya Kesarapu <soumyacole5566@gmail.com>
2021-07-17 00:52:21 +0530

    Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,34 @@
-import './App.css';
-import './main.css';
-import Playeritems from './Components/player/Playeritems';
-import Navbar from './Components/Layout/Navbar';
-import Playerform from './Components/player/Playerform';
-import Playersstate from './Context/Players/PlayersState';
+import "./App.css";
+import "./main.css";
+import React, { Fragment } from "react";
+import Playeritems from "./Components/player/Playeritems";
+import Navbar from "./Components/Layout/Navbar";
+import Playerform from "./Components/player/playerform";
+import Playersstate from "./Context/Players/PlayersState";
 // import Landing from './Components/Layout/Landing';
 // import Login from "./Components/Auth/Login";
-import Register from './Components/Auth/Register';
+import Register from "./Components/Auth/Register";
 // import Tabs from "./Components/Layout/Tabs";
-
-import Login from './Components/Auth/Login';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./Components/Auth/Login";
 
 function App() {
   return (
     <Playersstate>
-      <div className='navigation'>
-        <Navbar />
-        {/* <Tabs /> */}
-      </div>
-      <Playerform />
-      <Playeritems />
-      <Register />
-      <Login />
+      <Router>
+        <Fragment>
+          <div className="navigation">
+            <Navbar />
+            {/* <Tabs /> */}
+          </div>
+
+          <Switch>
+            <Route exact path="/" component={Playerform} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+          </Switch>
+        </Fragment>
+      </Router>
     </Playersstate>
   );
 }
--- client/src/Context/Players/PlayersState.js
+++ client/src/Context/Players/PlayersState.js
@@ -1,39 +1,39 @@
-import React, { useReducer } from 'react';
-import playerContext from './playerContext';
-import playerReducer from './playerReducer';
+import React, { useReducer } from "react";
+import playerContext from "./playerContext";
+import playerReducer from "./playerReducer";
 
 const PlayerState = (props) => {
   const initialState = {
     players: [
       {
-        name: 'Mason Greenwood',
-        playerNumber: '11',
-        role: 'Striker',
-        status: 'Substitute',
+        name: "Mason Greenwood",
+        playerNumber: "11",
+        role: "Striker",
+        status: "Substitute",
       },
       {
-        name: 'Harry Kane',
-        playerNumber: '9',
-        role: 'Striker',
-        status: 'Playing',
+        name: "Harry Kane",
+        playerNumber: "9",
+        role: "Striker",
+        status: "Playing",
       },
       {
-        name: 'Heung Min Son',
-        playerNumber: '7',
-        role: 'Left Mid',
-        status: 'Playing',
+        name: "Heung Min Son",
+        playerNumber: "7",
+        role: "Left Mid",
+        status: "Playing",
       },
       {
-        name: 'Lionel Messi',
-        playerNumber: '10',
-        role: 'Midfielder',
-        status: 'Rest',
+        name: "Lionel Messi",
+        playerNumber: "10",
+        role: "Midfielder",
+        status: "Rest",
       },
       {
-        name: 'Christiano Ronaldo',
-        playerNumber: '7',
-        role: 'Striker',
-        status: 'Playing',
+        name: "Christiano Ronaldo",
+        playerNumber: "7",
+        role: "Striker",
+        status: "Playing",
       },
     ],
   };
@@ -44,7 +44,8 @@ const PlayerState = (props) => {
     <playerContext.Provider
       value={{
         players: state.players,
-      }}>
+      }}
+    >
       {props.children}
     </playerContext.Provider>
   );
