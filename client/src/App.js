370aaa8c352d57753963a449dcb2d288a987a139
raviSridhar55 <61363065+raviSridhar55@users.noreply.github.com>
2021-07-19 22:20:02 +0530

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,31 @@
-import "./App.css";
-import "./main.css";
-import React, { Fragment } from "react";
-import Playeritems from "./Components/player/Playeritems";
-import Navbar from "./Components/Layout/Navbar";
-import Playerform from "./Components/player/playerform";
-import Playersstate from "./Context/Players/PlayersState";
+import './App.css';
+import './main.css';
+import React, { Fragment } from 'react';
+import Playeritems from './Components/player/Playeritems';
+import Navbar from './Components/Layout/Navbar';
+import Playerform from './Components/player/playerform';
+import Playersstate from './Context/Players/PlayersState';
 // import Landing from './Components/Layout/Landing';
 // import Login from "./Components/Auth/Login";
-import Register from "./Components/Auth/Register";
+import Register from './Components/Auth/Register';
 // import Tabs from "./Components/Layout/Tabs";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Login from "./Components/Auth/Login";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './Components/Auth/Login';
 
 function App() {
   return (
     <Playersstate>
       <Router>
         <Fragment>
-          <div className="navigation">
+          <div className='navigation'>
             <Navbar />
             {/* <Tabs /> */}
           </div>
 
           <Switch>
-            <Route exact path="/" component={Playerform} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/register" component={Register} />
+            <Route exact path='/' component={Playerform} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/register' component={Register} />
           </Switch>
         </Fragment>
       </Router>
