924c9e448be619b68e50087b8a3cdd5ff329d45d
Tanish Vemu <www.vtani74@gmail.com>
2021-07-22 01:37:35 +0530

    auth completed

--- client/src/Components/Auth/Login.js
+++ client/src/Components/Auth/Login.js
@@ -1,9 +1,44 @@
-import React from 'react';
-import LogoV from '../../img/Logo-V.png';
-const Login = () => {
+import React, { useEffect, useState, useContext } from "react";
+import LogoV from "../../img/Logo-V.png";
+import AuthContext from "../../Context/Auth/authContext";
+
+const Login = (props) => {
+  const authContext = useContext(AuthContext);
+
+  const { isAuthenticated, error, clearErrors, login } = authContext;
+
+  useEffect(() => {
+    if (isAuthenticated) props.history.push("/");
+    if (error === "Invalid Password" || error === "Invalid Team") {
+      alert("Invalid Credentials");
+      clearErrors();
+    }
+  }, [props.history, clearErrors, isAuthenticated, error]);
+
+  const [coach, setCoach] = useState({
+    teamName: "",
+    password: "",
+  });
+
+  const { teamName, password } = coach;
+
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log('object');
+    if (teamName === "" || password === "") {
+      alert("Please enter all credentials");
+    } else {
+      login({
+        teamName,
+        password,
+      });
+    }
+  };
+
+  const onChange = (e) => {
+    setCoach({
+      ...coach,
+      [e.target.name]: e.target.value,
+    });
   };
 
   return (
@@ -16,15 +51,30 @@ const Login = () => {
           <form
             className='add-form'
             onSubmit={onSubmit}
-            style={{ height: '400px' }}>
-            <h1 style={{ textAlign: 'center', color: 'black' }}>Login</h1>
+            style={{ height: "400px" }}
+          >
+            <h1 style={{ textAlign: "center", color: "black" }}>Login</h1>
             <div className='form-control'>
               <label>TEAM NAME</label>
-              <input type='text' placeholder='Team Name' />
+              <input
+                type='text'
+                placeholder='Team Name'
+                value={teamName}
+                name='teamName'
+                onChange={onChange}
+                required
+              />
             </div>
             <div className='form-control'>
               <label>PASSWORD</label>
-              <input type='password' placeholder='password' />
+              <input
+                type='password'
+                placeholder='Password'
+                value={password}
+                name='password'
+                onChange={onChange}
+                required
+              />
             </div>
             <button className='btn btn-block'>Login</button>
           </form>
--- client/src/Components/Layout/Home.js
+++ client/src/Components/Layout/Home.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
 import Playerform from "../player/Playerform";
 import Playeritems from "../player/Playeritems";
+import AuthContext from "../../Context/Auth/authContext";
 
 const Home = () => {
+  const authContext = useContext(AuthContext);
+
+  useEffect(() => {
+    authContext.loadUser();
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <div>
       <Playerform />
